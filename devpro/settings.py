45f7bf5cb4476f0c264691e5860c4cb7ed1514f5
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-09 01:46:42 -0300

    Configuração de arquivos estáticos.
    close #44

--- devpro/settings.py
+++ devpro/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# configura do ambiente de desenvolvimento
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
