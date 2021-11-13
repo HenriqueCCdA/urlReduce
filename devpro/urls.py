bb3c4a39af697317854a886e323e37f93ba9c3eb
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-13 01:16:19 -0300

    Mudando a rota dos links reduzidos.
    close #69

--- devpro/urls.py
+++ devpro/urls.py
@@ -21,6 +21,6 @@ from devpro.encurtador import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home_html'),
-    path('reduzido/<slug:slug>', views.redirecionar, name='redirecionar_name'),
-    path('relatorios/<slug:slug>', views.relatorios, name='relatio_name')
+    path('<slug:slug>', views.redirecionar, name='redirecionar_name'),
+    path('relatorio/<slug:slug>', views.relatorios, name='relatio_name')
 ]
