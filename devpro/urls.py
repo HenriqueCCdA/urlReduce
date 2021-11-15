f050f5915bab53fd5c2b77adacd3d56aec2dc67b
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-14 22:59:07 -0300

    Mudando a rota do admin.
    close #76

--- devpro/urls.py
+++ devpro/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from devpro.encurtador import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django/admin/', admin.site.urls),
     path('', views.home, name='home_html'),
     path('<slug:slug>', views.redirecionar, name='redirecionar_name'),
     path('relatorio/<slug:slug>', views.relatorio, name='relatorio_name')
