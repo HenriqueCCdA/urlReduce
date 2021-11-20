28f944039d3dd5d6370bfb3c1afa9fda31bf7edd
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-20 01:04:29 -0300

    Possibilidade de excluir um urls já cadastrada.
    close #95

--- devpro/encurtador/admin.py
+++ devpro/encurtador/admin.py
@@ -15,8 +15,5 @@ class UrlLog(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None) -> bool:
         return False
 
-    def has_delete_permission(self, request, obj=None) -> bool:
-        return False
-
     def has_change_permission(self, request, obj=None) -> bool:
         return False
